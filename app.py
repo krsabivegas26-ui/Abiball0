2a84ec25360fa4b54ce2d84e832fc91565574c39
krsabivegas26-ui <krs.abivegas26@gmail.com>
2026-05-24 17:45:29 +0200

    Reset Test 2

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ SMTP_HOST  = os.environ.get('SMTP_HOST', 'smtp.gmail.com')
 SMTP_PORT  = int(os.environ.get('SMTP_PORT', '587'))
 SMTP_USER  = os.environ.get('SMTP_USER', '')
 SMTP_PASS  = os.environ.get('SMTP_PASS', '')
-ADMIN_PW   = os.environ.get('ADMIN_PW',  'AbiVegas2026')
+ADMIN_PW   = os.environ.get('ADMIN_PW',  'AbiVegas2026admin')
 EMAIL_ENABLED = bool(SMTP_USER and SMTP_PASS)
 
 TABLES = [
